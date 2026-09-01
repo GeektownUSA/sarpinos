f5f2218cea792863c219ba4ba730a001565bbd47
GTU <45077650+GeektownUSA@users.noreply.github.com>
2026-09-01 19:20:16 -0400

    Update description for Sarpino's Pizzeria

--- app/menu/salads/page.js
+++ app/menu/salads/page.js
@@ -62,7 +62,7 @@ export default async function Page() {
         "url": `https://www.gosarpinos.com/menu/${postType}`,
         "image": heroImage ? heroImage.sourceUrl : '/default-menu-image.svg',
         "name": "Sarpino\'s Pizzeria",
-        "description": "Sarpino's is your go-to for authentic Italian flavor and free delivery on gourmet pizzas, open late into the night when, and where, you need it most.",
+        "description": "Sarpino's is your go-to for authentic Italian flavor and fast delivery on gourmet pizzas, open late into the night when, and where, you need it most.",
         "address": {
           '@type': 'PostalAddress',
           "streetAddress": '200 Tri State International, Suite 550',
