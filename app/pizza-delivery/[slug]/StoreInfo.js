610ce0b682389afb363320d7ff2cef296a282ec0
GTU <45077650+GeektownUSA@users.noreply.github.com>
2026-09-01 19:16:41 -0400

    Update delivery descriptions to emphasize speed

--- app/pizza-delivery/[slug]/StoreInfo.js
+++ app/pizza-delivery/[slug]/StoreInfo.js
@@ -81,11 +81,11 @@ const StoreInfo = ({ post }) => {
         <h3>Looking for the Best Pizza Delivery in {post.acf?.city || 'your city'}?</h3>
         <p>Look no further than your local Sarpino&apos;s. You want the best pizza delivery in your area, and we aim to fulfill that with hand-made pizza delivered to you with no delivery fee and, oh yeah, we&apos;re open late.</p>
         <p>We offer specialty pizza with the freshest of ingredients. Our menu is full of gourmet pizza options born from Italian tradition. We also provide numerous vegan/gluten free pizza options because everyone deserves great pizza in their life. Vegan pizza doesn&apos;t mean less flavor. Our vegan pizza uses fresh ingredients from trusted brands such as Daiya Cheese, Beyond Meat, and Field Roast to ensure maximum flavor. We encourage you to try all our different vegan pizza options in Sarpino&apos;s Pizzeria {post.acf?.city || 'your city'}. We can even accommodate new creations, so feel free to order a customized vegan pizza today!</p>
-        <p>Our menu provides so much more than pizza, serving up sandwiches, pastas, calzones, salads, sides, desserts and more! All meals are made fresh and you can also count on free delivery when you order from Sarpino&apos;s online.</p>
+        <p>Our menu provides so much more than pizza, serving up sandwiches, pastas, calzones, salads, sides, desserts and more! All meals are made fresh and you can also count on fast delivery when you order from Sarpino&apos;s online.</p>
       </div>
       <div className={styles.content}>
-        <h5>The Only Choice for Free Delivery in the {post.acf?.metro_area || 'your metro area'} metro area.</h5>
-        <p>We take pizza and food delivery to the next level. You get fast, free food delivery, even if it&apos;s a late night. There is no minimum order and you can take your time while using our online menu. And of course, you can always count on getting delicious pizza from Sarpino&apos;s Pizzeria in the {post.acf?.metro_area || 'your metro area'} metro area.</p>
+        <h5>The Only Choice for Fast Delivery in the {post.acf?.metro_area || 'your metro area'} metro area.</h5>
+        <p>We take pizza and food delivery to the next level. You get fast food delivery, even if it&apos;s a late night. There is no minimum order and you can take your time while using our online menu. And of course, you can always count on getting delicious pizza from Sarpino&apos;s Pizzeria in the {post.acf?.metro_area || 'your metro area'} metro area.</p>
       </div>
       <div className={styles.content}>
         <h5>Wondering about our delivery area?</h5>
@@ -107,8 +107,8 @@ const StoreInfo = ({ post }) => {
         <h5>You can get hot, fresh food delivered to your door in three easy steps:</h5>
         <ol className={styles.list}>
           <li>Browse {he.decode(post.title.rendered)} <OrderLink label="online menu" /> and pick what you like from our selection of pizzas, pastas, sandwiches, and more.</li>
-          <li>Order online using our secure website. There is no minimum order requirement for free delivery.</li>
-          <li>We will deliver your food fast and free right to your door.</li>
+          <li>Order online using our secure website. There is no minimum order requirement for fast delivery.</li>
+          <li>We will deliver your food fast right to your door.</li>
         </ol>
       </div>
       <OrderBtn />
